ce2e4f1bc2b48a112b098f62de6c6f1bce1dc629
Alina Pellja <alina pellja@gmail.com>
2025-05-09 02:55:30 +0300

    menu links added

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const btnHamburger = document.querySelector('.hamburger-menu');
 const menuPanel = document.querySelector('.menu-panel');
+const menuLinks = document.querySelectorAll('.menu-panel a');
 
 btnHamburger.addEventListener('click', (event) => {
-    console.log(menuPanel.classList);
+    
     menuPanel.classList.toggle('active');
-    console.log(menuPanel.classList);
+    
  }
+);
+document.addEventListener('click', (event) => {
+    if (!menuPanel.contains(event.target) && !btnHamburger.contains(event.target)) {
+        menuPanel.classList.remove('active');
+    }
+}
+);
+
+menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        menuPanel.classList.remove('active');
+    });
+}
 );
